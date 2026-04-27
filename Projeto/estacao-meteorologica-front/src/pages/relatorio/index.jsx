23db60040843ef4896653289c9ca83f12c3df58f
Eloa <eloa.r.santos@aluno.senai.br>
2026-04-27 10:43:01 -0300

    Relatório e inicio Grafico

---
+++ Projeto/estacao-meteorologica-front/src/pages/relatorio/index.jsx
@@ -0,0 +1,52 @@
+import Header from '../../components/header';
+import './relatorio.css';
+
+
+export default function Relatorio(){
+    const leituras = [
+        {horario: "12:00", qualidadeAr: "Boa", iqa: 42, temperatura: "26°C", umidade: "68%"},
+        {horario: "13:00", qualidadeAr: "Boa", iqa: 41, temperatura: "28°C", umidade: "65%"},
+        {horario: "14:00", qualidadeAr: "Boa", iqa: 42, temperatura: "28°C", umidade: "65%"},
+        {horario: "15:00", qualidadeAr: "Média", iqa: 39, temperatura: "30°C", umidade: "65%"},
+        {horario: "16:00", qualidadeAr: "Boa", iqa: 41, temperatura: "28°C", umidade: "67%"}
+    ]
+
+    return(
+        <div className='container'>
+            <Header/>
+            <h3>Relatório Estação Meteorológica </h3>
+            <p>Monitoramento da temperatura e umidade em tempo real</p>
+            <section className='graficos'> {/*colocar graficos*/}
+            </section>
+
+            <section className='tabela-leituras'>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Horário</th>
+                            <th>Qualidade ar</th>
+                            <th>IQA</th>
+                            <th>Temperatura</th>
+                            <th>Umidade</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            leituras.map
+                            ((item, index) =>(
+                                <tr key={index}>
+                                    <td>{item.horario}</td>
+                                    <td>{item.qualidadeAr}</td>
+                                    <td>{item.iqa}</td>
+                                    <td>{item.temperatura}</td>
+                                    <td>{item.umidade}</td>
+                                </tr>
+                            ))
+                        }
+                    </tbody>
+                </table>
+            </section>
+        </div>
+        
+    )
+}
